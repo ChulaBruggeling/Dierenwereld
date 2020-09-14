0920cf577b964ba7bc99c611278c4fb6a5f7c0f2
Chula Bruggeling <chula.bruggeling@gmail.com>
2020-09-14 10:06:00 +0200

    Dieren starten op random startpositie

--- Model/Dier.cs
+++ Model/Dier.cs
@@ -7,10 +7,17 @@ namespace Model
     public class Dier
     {
         public string DrawSign = "*";
-        public int x = 0;
-        public int y = 0;
+        public int x;
+        public int y;
         public int xRichting = 1;
         public int yRichting = 1;
+
+        public Dier()
+        {
+            Random randm = new Random();
+            x = randm.Next(0, 10);
+            y = randm.Next(0, 10);
+        }
 
         public virtual void Bewegen()
         {
